14594d43245141d8bc0a00f6399cb6cf23a5c2a8
rick.van.baalen <rick@ijzersterkproducties.com>
2025-05-03 11:10:55 +0200

    Remove unused variable

--- src/main/java/com/pingpal/pingpal_backend/controller/AuthController.java
+++ src/main/java/com/pingpal/pingpal_backend/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.pingpal.pingpal_backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.*;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import com.pingpal.pingpal_backend.util.JwtUtil;
@@ -17,7 +16,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
-        Authentication authentication = authManager.authenticate(
+        authManager.authenticate(
             new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
         );
 
